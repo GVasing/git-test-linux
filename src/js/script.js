51e1abe847fa48c0f36ac2fd4c8dfadc98b27bb4
George Vasiliadis <gvasiliadis72@gmail.com>
2025-06-18 16:32:48 +1000

    showMessage function added for user interface.

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,7 @@ console.log("Git workflow practice!");
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Page loaded succesfully");
 });
+
+function showMessage() {
+    alert("Git is awesome");
+}
